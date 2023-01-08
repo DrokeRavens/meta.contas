e82b000abc3f7df6ddb397e90be3cad70d505ab5
DrokeRavens <droke.ravens@gmail.com>
2023-01-08 10:04:11 -0300

    interface basica, adicionar e listar contas

--- Contas.Web/contas-web/src/lib/services/contasService.ts
+++ Contas.Web/contas-web/src/lib/services/contasService.ts
@@ -1,7 +1,13 @@
+import type { CreateContaDto } from '$lib/models/createContaDto';
 import apiClient from './api';
 
-export class ContasService {
-	static async getContas() {
+class ContasService {
+	async createConta(createContaModel: CreateContaDto) {
+		return apiClient.post('/contas', createContaModel);
+	}
+	async getContas() {
 		return apiClient.get('/contas');
 	}
 }
+
+export const contasService = new ContasService();
